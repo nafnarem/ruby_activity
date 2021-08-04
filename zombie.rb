f35e66a11b29879311dcb1687034659cac0b90f3
root <root@Nafnarem.localdomain>
2021-08-04 21:49:11 +0800

    Profile

--- zombie.rb
+++ zombie.rb
@@ -1,4 +1,7 @@
 class Zombie
+  attr_reader :name, :type 
+  attr_writer :name, :type 
+  attr_accessor :gender, :origin
   def initialize (name, type, gender, origin)
     @name = name
     @type = type
@@ -6,7 +9,7 @@ class Zombie
     @origin = origin
   end
   def eat_brainz
-    puts "Hey #{@name}! Human brains to your liking as #{@origin}"
+    puts "Hey #{@name}! Human brains to your liking as a #{@type} one?"
   end
   def invade_world
     puts "#{@type} cares not for world conquest"
@@ -19,3 +22,6 @@ zombie2 = Zombie.new("Akrash", "Parasitic", "male", "alien")
 
 zombie2.eat_brainz
 zombie1.invade_world
+puts zombie1.name
+zombie1.name = "browny"
+puts zombie1.name
