8b6c466beeecd74653a20eabda4d342bfd454d58
root <root@Nafnarem.localdomain>
2021-08-05 20:40:01 +0800

    Code Aligned

--- arr_check_divisible.rb
+++ arr_check_divisible.rb
@@ -3,9 +3,9 @@ arr = [6, 3, 1, 8, 4, 2, 10, 65, 102]
 num = gets.chomp.to_i
 
 if num%2 == 0
-    new_arr = arr.map{|new| new}
-    new_arr.push(num)
-    puts "new array is as follows #{new_arr}"
+  new_arr = arr.map{|new| new}
+  new_arr.push(num)
+  puts "new array is as follows #{new_arr}"
 else puts "#{num} not divisible by 2"
 end
 
--- array_include.rb
+++ array_include.rb
@@ -2,7 +2,7 @@ arr = [1, 3, 5, 7, 9, 11]
 number = 3
 
 arr.each do |num|
-    if num == number
-      puts "#{number} is in the array."
-    end
+  if num == number
+    puts "#{number} is in the array."
   end
+end
