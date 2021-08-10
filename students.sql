9263abe1e1336c863da1aa3525780edf78a61b52
nafnarem <johnparreno@gmail.com>
2021-08-10 20:09:24 +0800

    Modified and updated assignment

--- students.sql
+++ students.sql
@@ -1,24 +1,32 @@
 CREATE TABLE students (
   id integer PRIMARY KEY NOT NULL,
   first_name VARCHAR(50) NOT NULL,
-  middle_name VARCHAR(50) NOT NULL,
+  middle_name VARCHAR(50) DEFAULT 'Blank',
   last_name VARCHAR(50) NOT NULL,
   age integer NOT NULL,
   location VARCHAR(50) NOT NULL
 );
 
 INSERT INTO 
-  students(id, first_name, middle_name, last_name, age, location)
+  students(id, first_name, last_name, age, location)
 VALUES
-  (1, 'Jose', 'Libo-on', 'Parreno', 67, 'Afterlife'),
-  (2, 'Carmen', 'Villanueva', 'Parreno', 68, 'Bacolod City'),
-  (3, 'Valerie', 'Parreno', 'Alova', 40, 'Iloilo City'),
-  (4, 'Kathryn Kristy', 'Parreno', 'Bautista', 38, 'Bacolod City'),
-  (5, 'Charmaine Claire', 'Parreno', 'Nguyen', 33, 'California'),
-  (6, 'John Raymund', 'Villanueva', 'Parreno', 31, 'Bacolod City');
+  (1, 'Juan', 'Cruz', 18, 'Manila'),
+  (2, 'Anne', 'Wall', 20, 'Manila'),
+  (3, 'Theresa', 'Joseph', 21, 'Manila'),
+  (4, 'Isaac', 'Grey', 18, 'Laguna'),
+  (5, 'Zack', 'Mathews', 33, 'Marikina'),
+  (6, 'Finn', 'Lam', 25, 'Manila');
 
 UPDATE students
 SET first_name = 'Ivan', middle_name= 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan' 
 WHERE id = 1;
 
 DELETE FROM students WHERE id = 6;
+
+SELECT COUNT(ID) FROM students;
+
+SELECT * FROM students WHERE location = 'Manila'; 
+
+SELECT AVG(age) from students;
+
+SELECT * FROM students ORDER BY age desc;
